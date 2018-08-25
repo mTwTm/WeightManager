bc46790f40edf1e6f38c1ad7c0fcc24e02872f9c
Teng-pao Yu <git@mtwtm.org>
2018-08-25 23:29:48 +0800

    Show dialog when fab clicked

--- app/src/main/java/org/mtwtm/weightmanager/MainActivity.java
+++ app/src/main/java/org/mtwtm/weightmanager/MainActivity.java
@@ -1,15 +1,29 @@
 package org.mtwtm.weightmanager;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static class NewWeightFragment extends DialogFragment {
+
+        @Override
+        @Nullable
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_add_weight, container, true);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                DialogFragment dialogFragment = new NewWeightFragment();
+                dialogFragment.show(getSupportFragmentManager(), "Whatever");
             }
         });
     }
